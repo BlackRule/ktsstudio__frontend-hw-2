323594b101bf93c7f2179ae53f7c80de3f7056c5
Your Name <you@example.com>
2022-08-29 02:14:00 +0200

    aliases for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const {alias, configPaths} = require("react-app-rewire-alias");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -13,5 +16,17 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    const overrider = alias(configPaths("./tsconfig.paths.json"));
+    return overrider(config);
   }
 }
+
+
