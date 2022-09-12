74796622d41c25299ce66d5332ad7592754ed81a
Your Name <you@example.com>
2022-09-13 01:32:22 +0200

    moved App component to separate file index.scss is no longer a module updated Plan.txt

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import styles from './index.module.scss'
-import Products from "@pages/Products";
-import Product from "@pages/Product";
-import {Header} from "@components/Header/Header";
+import {BrowserRouter} from 'react-router-dom';
+import './index.scss'
 import '@config/configureMobX'
-import {useQueryParamsStoreInit} from "@store/RootStore/hooks/useQueryParamsStoreInit";
+import App from "@components/App/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-
-const App=()=>{
-    useQueryParamsStoreInit()
-    return (
-        <div className={styles.App}>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Products />} />
-                <Route path="/product">
-                    <Route path=':id' element={<Product/>}/>
-                    <Route path="" element={<Navigate to="/404" replace />} />
-                </Route>
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </div>
-    )
-}
 
 root.render(
     <BrowserRouter>
